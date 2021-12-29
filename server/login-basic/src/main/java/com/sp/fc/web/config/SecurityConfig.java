695dd4115de02b61cdb63ab31611647e2869339e
chips-gguma <ljygguma@gmail.com>
2021-12-29 18:43:56 +0900

    root page는 권한 허가, 허용에 대해서는 인증 웹 리소스에 대해서는 security filter를 거치지 않도록 configure override

--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.sp.fc.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,9 +15,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests(request->{
                     request
-                            .antMatchers("/**").permitAll()
-                            ;
+                            .antMatchers("/").permitAll() // root 페이지는 모든 사람들의 권한 허용
+                            .anyRequest().authenticated() // 요청은 인증 받은 사람만 (보안)
+                    ;
                 })
+                ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring() // 웹 리소스에 대해서는 시큐리티 filter가 적용되지 않도록
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations() // 디버깅을 걸고 보면 css, js 파일 내려받는 것 확인 가능
+                )
                 ;
     }
 }
